a2b2c917da49f011180b8d9ca3f408da86f13722
accofankush <emailfankush@gmail.com>
2021-05-22 21:57:33 +0530

    fixed sidebar Roles

--- src/components/dashboard/Sidebar.jsx
+++ src/components/dashboard/Sidebar.jsx
@@ -13,31 +13,55 @@ function Sidebar() {
 			path: "/dashboard",
 			subpaths: ["user/"],
 			icon: <HomeIcon />,
+			access: ["Admin", "Cashier"],
 		},
-		{ label: "Users", path: "/users", icon: <PeopleAltIcon /> },
-		{ label: "Category", path: "/categories", icon: <PeopleAltIcon /> },
-		{ label: "Products", path: "/products", icon: <PeopleAltIcon /> },
-		{ label: "Transactions", path: "/transactions", icon: <PeopleAltIcon /> },
+		{
+			label: "Users",
+			path: "/users",
+			icon: <PeopleAltIcon />,
+			access: ["Admin"],
+		},
+		{
+			label: "Category",
+			path: "/categories",
+			icon: <PeopleAltIcon />,
+			access: ["Admin"],
+		},
+		{
+			label: "Products",
+			path: "/products",
+			icon: <PeopleAltIcon />,
+			access: ["Admin", "Cashier"],
+		},
+		{
+			label: "Transactions",
+			path: "/transactions",
+			icon: <PeopleAltIcon />,
+			access: ["Admin", "Cashier"],
+		},
 	];
 	return (
 		<div className="sidebar">
-			{sidebarLinks.map((link) => (
-				<Link to={link.path}>
-					<div
-						className={
-							link.path === location.pathname ||
-							(link.subpaths &&
-								link.subpaths.find((link) =>
-									location.pathname.startsWith(link),
-								))
-								? "sidebar-link active"
-								: "sidebar-link"
-						}>
-						{link.icon}
-						<h6>{link.label}</h6>
-					</div>
-				</Link>
-			))}
+			{sidebarLinks.map(
+				(link) =>
+					link.access.includes(localStorage.getItem("role")) && (
+						<Link to={link.path}>
+							<div
+								className={
+									link.path === location.pathname ||
+									(link.subpaths &&
+										link.subpaths.find((link) =>
+											location.pathname.startsWith(link),
+										))
+										? "sidebar-link active"
+										: "sidebar-link"
+								}>
+								{link.icon}
+								<h6>{link.label}</h6>
+							</div>
+						</Link>
+					),
+			)}
 		</div>
 	);
 }
